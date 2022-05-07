fb3f2a2e60b8c0b706ca8cb52e1ca8721c00d6eb
Hyunwoo Kim <admin@hyunwoo.kim>
2022-05-07 16:21:38 +0900

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,39 +1,16 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Trending from '@/views/Trending.vue';
-import Articles from '@/views/Articles.vue';
-import War from '@/views/War.vue';
-import Protest from '@/views/Protest.vue';
+import Home from '@/views/Home.vue'
 
-import { mdiFlag, mdiTank, mdiTrendingUp, mdiNewspaperVariantOutline } from '@mdi/js';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Trending 🔥',
-    component: Trending,
-    meta: { icon: mdiTrendingUp },
-  },
-  {
-    path: '/articles',
-    name: 'Articles',
-    component: Articles,
-    meta: { icon: mdiNewspaperVariantOutline },
-  },
-  {
-    path: '/war',
-    name: 'War',
-    component: War,
-    meta: { icon: mdiTank },
-  },
-  {
-    path: '/protest',
-    name: 'Protest',
-    component: Protest,
-    meta: { icon: mdiFlag },
-  },
+    name: 'Home',
+    component: Home
+  }
 ];
 
 const router = new VueRouter({
